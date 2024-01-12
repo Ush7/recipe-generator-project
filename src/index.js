311a2dadb1be61176258885c90f22dacabf0613e
Ush7 <ush.shilp@gmail.com>
2024-01-12 00:09:20 -0500

    Added js structure to website

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+function generateRecipe(event) {
+  event.preventDefault();
+  new Typewriter("#resultRecipe", {
+    strings:
+      "🍝 Your recipe will be generated here,🧂 Your ingredients will be generated here",
+    autoStart: true,
+    cursor: null,
+    delay: 10,
+  });
+}
 
+let recipeGeneratorformElement = document.querySelector(
+  "#recipe-generate-form"
+);
+recipeGeneratorformElement.addEventListener("submit", generateRecipe);
